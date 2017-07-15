32620fd16b71b189bfe07ac73e9ba53f7afc69df
adil <adil.farhan@gmail.com>
2017-07-15 14:29:53 +0530

    fourth commit

--- src/ScalaDemo2.scala
+++ src/ScalaDemo2.scala
@@ -2,7 +2,7 @@
 
 object ScalaDemo2 extends App {
 
-  val g = new ScalaExample ("hiiiiiii hello asasa dadadadad")
+  val g = new ScalaExample ("hiiiiiii hello asasa dadadadad asasa")
   g.greet()
 
 }
