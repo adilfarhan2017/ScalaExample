46e56eab239d6a11790ecc354f6d321612fdf22c
adilfarhan2017 <adil.farhan@gmail.com>
2017-09-30 21:36:31 +0530

    Update ScalaDemo.scala

--- src/ScalaDemo.scala
+++ src/ScalaDemo.scala
@@ -15,7 +15,7 @@ object ScalaDemo extends App {
 
   var greetStrings = new Array[String](3)
   greetStrings(0) = "Hi"
-  greetStrings(1) = "How are you ?"
+  greetStrings(1) = "How are you and where are you ?"
   greetStrings(2)= "adil Farhan\n"
 
   greetStrings.foreach(println)
